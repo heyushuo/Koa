256227cb3ba68db39e53d4eb536b253643f194e1
heyushuo <1007561607@qq.com>
2018-07-20 15:57:45 +0800

    mysql

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONF={
+    mysql:{
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        db: 'nodemysql',
+        pass: '123456',
+        char: 'utf8mb4'
+    }
+}
+
+module.exports=CONF;
--- index.js
+++ index.js
@@ -1,13 +1,94 @@
 const koa = require('koa');
-const app = new koa();
 
+// 注意require('koa-router')返回的是函数:
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
+
+const app = new koa();
+// add router middleware:
+app.use(router.routes());
+app.use(bodyParser());
+
+
+
+
+/**
+ * 数据库操作
+ */
+const { mysql: config } = require('./config');
+
+
+//链接数据库
+const DB = require('knex')({
+    client: 'mysql',
+    connection: {
+        host: config.host,
+        port: config.port,
+        user: config.user,
+        password: config.pass,
+        database: config.db,
+        charset: config.char,
+        multipleStatements: true
+    }
+})
+DB.select('*').from('nideshop_goods').then(function(data){
+    //取到的数据
+    console.log(data)
+})
+/**
+ * 数据库操作
+ */
+
+
+
+// 参数ctx是由koa传入的封装了request和response的变量，我们可以通过它访问request和response，next是koa传入的将要处理的下一个异步函数。
+
+// 上面的异步函数中，我们首先用await next();处理下一个异步函数，然后，设置response的Content-Type和内容。
+
+// 由async标记的函数称为异步函数，在异步函数中，可以用await调用另一个异步函数，这两个关键字将在ES7中引入。
 
 // 对于任何请求，app将调用该异步函数处理请求：
+
+// 每收到一个http请求，koa就会调用通过app.use()注册的async函数，并传入ctx和next参数。
+
+// 我们可以对ctx操作，并设置返回内容。但是为什么要调用await next()？
+
+// 原因是koa把很多async函数组成一个处理链，每个async函数都可以做一些自己的事情，然后用await next()来调用下一个async函数。我们把每个async函数称为middleware，这些middleware可以组合起来，完成很多有用的功能。
+// 最后注意ctx对象有一些简写的方法，例如ctx.url相当于ctx.request.url，ctx.type相当于ctx.response.type。
+// app.use(async (ctx, next) => {
+//     await next();
+//     // 设置response的Content-Type:
+//     ctx.response.type = 'text/html'
+//     ctx.body = "<h1>hell world23m</h1>"
+// })
+
+
 app.use(async (ctx, next) => {
-    await next();
-    // 设置response的Content-Type:
-    ctx.response.type = 'text/html'
-    ctx.body = "<h1>hell world23m</h1>"
+    console.log(`${ctx.request.method} ${ctx.request.url}1`); // 打印URL
+    await next(); // 调用下一个middleware
+});
+
+// app.use(async (ctx, next) => {
+//     const start = new Date().getTime(); // 当前时间
+//     await next(); // 调用下一个middleware
+//     const ms = new Date().getTime() - start; // 耗费时间
+//     console.log(`Time: ${ms}ms`); // 打印耗费时间
+// });
+
+// app.use(async (ctx, next) => {
+//     await next(); //先执行这个
+//     ctx.response.type = 'text/html';
+//     ctx.response.body = '<h1>Hello, koa2!</h1>';
+// });
+
+router.get("/hello/:id", async (ctx, next) => {
+    var id = ctx.params.id;
+    ctx.response.type = 'text/html';
+    ctx.response.body = `<h1>这个id是 ${id}</h1>`;
+})
+router.get("/", async (ctx, next) => {
+    ctx.response.type = 'text/html';
+    ctx.response.body = `<h1>我是首页</h1>`;
 })
 
 app.listen(9991)
