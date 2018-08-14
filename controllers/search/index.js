f834a03e9cc0016fd1b32ca1ab9cd6d7a30f6843
heyushuo <1007561607@qq.com>
2018-08-14 23:31:38 +0800

    search

--- controllers/search/index.js
+++ controllers/search/index.js
@@ -1,11 +1,11 @@
 const {
   mysql
 } = require('../../mysql');
-async function indexAction() {
+async function indexAction(ctx) {
   // 默认关键词
   const defaultKeyword = await mysql('nideshop_keywords').where({
     is_default: 1
-  }).limit(1).elect();
+  }).limit(1).select();
   // 取出热闹关键词
   const hotKeywordList = await mysql('nideshop_keywords').distinct('keyword').column('keyword', 'is_hot').limit(10).select();
   // const historyKeywordList = await mysql('nideshop_search_history').distinct('keyword').where({
@@ -21,9 +21,16 @@ async function indexAction() {
 async function helperAction(ctx) {
   const keyword = ctx.query.keyword;
   const keywords = await mysql("nideshop_goods").column("name").where("name", 'like', '%' + keyword + '%').limit(10).select();
-  ctx.body = {
-    keywords
+  if (keyword) {
+    ctx.body = {
+      keywords
+    }
+  } else {
+    ctx.body = {
+      keywords: []
+    }
   }
+
 }
 // async function () {
 
